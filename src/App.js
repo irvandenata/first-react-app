3a15584bace42676a3261c649fb7417579cd46d4
Irvan Denata <38544210+irvandenata@users.noreply.github.com>
2024-01-18 08:27:31 +0700

    Update : Learning Mapping data with map method in javascript

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const data = [
+  {
+    id: 1,
+    name: 'John',
+    age: 20,
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    age: 21,
+  },
+  {
+    id: 3,
+    name: 'Joe',
+    age: 22,
+  },
+]
+
+const listItems = data.map((item) =>{
+  return {
+    key: item.id,
+    value: item.name + ' is ' + item.age + ' years old.'
+  }
+})
+
+
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +49,7 @@ function App() {
           Learn React
         </a>
       </header>
+      {listItems.map((item) => <ListItem key={item.key} value={item.value} />)}
     </div>
   );
 }
